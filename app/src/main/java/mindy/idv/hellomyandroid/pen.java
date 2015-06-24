f38d0b031053dd65f247bef6328dc40a1a296732
Mindy <androidtestbk@gmail.com>
2015-06-24 15:11:36 +0800

    the first test

---
+++ app/src/main/java/mindy/idv/hellomyandroid/pen.java
@@ -0,0 +1,33 @@
+package mindy.idv.hellomyandroid;
+
+/**
+ * Created by 201 on 2015/6/24.
+ */
+public class pen {
+    private String name;
+    private int price;
+
+    public pen() {
+    }
+
+    public pen(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
